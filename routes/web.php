74da994ecce45e21c4ac3bbcf2bbc1b711f0e40b
Danny <edanuc15@gmail.com>
2026-09-22 09:37:43 -0600

    agrupando de mejor forma las routes del proyecto

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\AreaController;
-use App\Http\Controllers\UserProfileController;  // Asegúrate de importar el controlador
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Models\Report;
@@ -21,22 +21,16 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name
 
 // Ruta de inicio después del login
 Route::middleware('auth')->get('/', function () {
-    $reports = Report::all();  // Obtén todos los reportes
-    return view('reports.index', compact('reports'));  // Apuntamos a reports/index.blade.php
+    $reports = Report::all();
+    return view('reports.index', compact('reports'));
 })->name('home');
 
 // Agrupación de rutas protegidas
 Route::middleware('auth')->group(function () {
-    // Rutas de reportes (usando resource para simplificar)
     Route::resource('reports', ReportController::class);
-
-    // Rutas de áreas
     Route::resource('areas', AreaController::class);
 
-    // Ruta del perfil del usuario
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
-        Route::put('/profile', [UserProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [UserProfileController::class, 'delete'])->name('profile.delete'); // Ruta para eliminar cuenta
-    });
+    Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
+    Route::put('/profile', [UserProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [UserProfileController::class, 'delete'])->name('profile.delete');
 });
